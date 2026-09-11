457cced9b97c7fac80b92d932c3a90a567b42cf3
Dylan <textilesdpl@gmail.com>
2026-09-10 22:56:12 -0500

    Fix puppeteer executable path via module

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const express = require('express');
+const puppeteer = require('puppeteer');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
+        executablePath: puppeteer.executablePath(),
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
@@ -14,7 +16,6 @@ const client = new Client({
         ]
     }
 });
-
 // Estado del torneo
 let torneo = {
     activo: false,
